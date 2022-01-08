a4dc068fe8277399babce2a306ddc0985ccf6212
Itamar Carvalho <itamarc@gmail.com>
2022-01-08 13:18:24 -0300

    Designed search form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Menu from './Menu';
+import SearchForm from './SearchForm';
 
 function Header() {
     return (
         <header>
             <h1><img src="/mml-logo120.png" alt="My Movies List" /> My Movies List</h1>
+            <SearchForm />
             <Menu />
         </header>
     );
